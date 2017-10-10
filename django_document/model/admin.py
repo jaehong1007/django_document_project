3184fe5d1a91601af50a4b03d05ff42b7ee39c50
Jaehong Kim <jaehong1007@gmail.com>
2017-10-10 14:06:57 +0900

    User class 추가

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from model.models import User, Car, Manufacturer
+
+admin.site.register(Car)
+admin.site.register(Manufacturer)
+admin.site.register(User)
--- django_document/model/models.py
+++ django_document/model/models.py
@@ -10,3 +10,37 @@ class Person(models.Model):
     )
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.name}'
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    pass
+
+
+class Pizza(models.Model):
