0544b15eb509f13a3f7328ccf2b9419fde63941f
Jaehong Kim <jaehong1007@gmail.com>
2017-10-10 15:30:39 +0900

    TwitterUser 추가

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 # from model.models import User, Car, Manufacturer
-from .models import Car, User, Manufacturer
-
+from model.models.many_to_many import TwitterUser
+from .models import Car, User, Manufacturer, Topping, Pizza
 
 admin.site.register(Car)
 admin.site.register(Manufacturer)
 admin.site.register(User)
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(TwitterUser)
--- django_document/model/models/many_to_many.py
+++ django_document/model/models/many_to_many.py
@@ -8,6 +8,10 @@ __all__ = (
 
 class Pizza(models.Model):
     name = models.CharField(max_length=30)
+    toppings = models.ManyToManyField(
+        'Topping',
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
@@ -15,6 +19,18 @@ class Pizza(models.Model):
 
 class Topping(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class TwitterUser(models.Model):
+
+    # 자기자신(Twitteruser ('self'))를 참조해서
+    # friends필드를 MTM으로 정의
+
+    name = models.CharField(max_length=30)
+    freinds = models.ManyToManyField('self', blank=True,)
 
     def __str__(self):
         return self.name
