43ec685a53e7c391cb699078d7b92de169272fef
Jaehong Kim <jaehong1007@gmail.com>
2017-10-11 16:36:44 +0900

    Place, Restaurant 추가

--- django_document/inheritance/admin.py
+++ django_document/inheritance/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Teacher, Student, School
+from .models import Teacher, Student, School, Restaurant, Place
 
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(School)
+admin.site.register(Place)
+admin.site.register(Restaurant)
